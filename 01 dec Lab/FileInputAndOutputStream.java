341eb38e02218e2679646f688ce17deceda91ad6
Prithula-Samanta <111556690+Prithula-Samanta@users.noreply.github.com>
2022-12-01 23:05:06 +0530

    Add files via upload

---
+++ 01 dec Lab/FileInputAndOutputStream.java
@@ -0,0 +1,46 @@
+package com.Lab;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+ class FileInputAndOutputStream {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner (System.in);
+    	System.out.println("Enter your choice ");
+    	System.out.println("Press 1 read the data from console and store into file ");
+    	System.out.println("Press 2 for print the data from the file ");
+    	
+int n=sc.nextInt();
+switch (n) {
+    case 1:
+    try {
+        FileOutputStream f = new FileOutputStream("D:\\test1.txt");
+        String str = "Welcome to Anudip Foundation";
+        byte b[] = str.getBytes(); // converting string into byte array
+        f.write(b);
+        f.close();
+        System.out.println("Welcome to Anudip Foundation");
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    break;
+    case 2:
+        try {
+            // Write Byte to string
+            FileInputStream f = new FileInputStream("D:\\test1.txt"); //destination
+            int i=0;
+            while((i=f.read())!=-1) {
+                System.out.print((char)i);
+
+            }
+            f.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        break;
+    default:
+        System.out.println("Wrong Input");
+}
+    }
+} 
+
---
+++ 01 dec Lab/FileIntoByteArray.java
@@ -0,0 +1,19 @@
+package com.Lab;
+
+import java.io.FileInputStream;
+
+class FileIntoByteArray{
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("D:\\test1.txt");
+            byte arr[]=new byte[1024];
+            int i=0;
+            while ((i=fin.read())>0){
+                System.out.print((char)i);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
